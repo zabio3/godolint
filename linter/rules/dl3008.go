6b425d6a0dbd40c450e1f00fe6faff0a07615131
zabio3 <zabio1192@gmail.com>
2019-03-10 21:56:53 +0900

    Modify DL3008
    filter `-y` `&&`

--- linter/rules/dl3008.go
+++ linter/rules/dl3008.go
@@ -15,13 +15,19 @@ func dl3008Check(node *parser.Node, file string) (rst []string, err error) {
 		if child.Value == "run" {
 			isAptGet, isInstall := false, false
 			for _, v := range strings.Fields(child.Next.Value) {
+				isRemoveStr := false
 				switch v {
 				case "apt-get":
 					isAptGet = true
 				case "install":
 					isInstall = true
 				default:
-					if isAptGet && isInstall && !verPattern.MatchString(v) {
+					for _, w := range []string{"-y", "&&"} {
+						if v == w {
+							isRemoveStr = true
+						}
+					}
+					if isAptGet && isInstall && !isRemoveStr && !verPattern.MatchString(v) {
 						rst = append(rst, fmt.Sprintf("%s:%v DL3008 Pin versions in apt get install. Instead of `apt-get install <package>` use `apt-get install <package>=<version>`\n", file, child.StartLine))
 					}
 				}
