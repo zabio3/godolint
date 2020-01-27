1e35c9054ba82936f7d67d1a21e4ef87f0aca861
Tomohiro Kusumoto <t.kusumoto0926@gmail.com>
2020-01-27 12:58:44 +0900

    fix dl3025

--- linter/rules/dl3025.go
+++ linter/rules/dl3025.go
@@ -1,31 +1,16 @@
 package rules
 
 import (
-	"strings"
-
 	"github.com/moby/buildkit/frontend/dockerfile/parser"
 )
 
 // validateDL3025 Use arguments JSON notation for CMD and ENTRYPOINT arguments
 func validateDL3025(node *parser.Node) (rst []ValidateResult, err error) {
 	for _, child := range node.Children {
-		var isErr bool
 		if (child.Value == ENTRYPOINT) || (child.Value == CMD) {
-			args := strings.Fields(child.Original)
-			length := len(args) - 1
-			for i, v := range strings.Fields(child.Original) {
-				switch i {
-				case 1:
-					if v[:1] != "[" {
-						isErr = true
-					}
-				case length:
-					if v[len(v)-1:] != "]" {
-						isErr = true
-					}
-				}
-			}
-			if isErr {
+			l := len(child.Value)
+			if child.Original[l+1:l+2] != "[" ||
+				child.Original[len(child.Original)-1:] != "]" {
 				rst = append(rst, ValidateResult{line: child.StartLine})
 			}
 		}
